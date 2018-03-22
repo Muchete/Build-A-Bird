aaca49c6ca28814d50fa68a68fa7c9c8c77ad5e4
TobiasDupuch <tobiasdupuch@Tobiass-MacBook-Pro.local>
2018-03-22 22:26:26 +0100

    Added texts

--- interface/script.js
+++ interface/script.js
@@ -1,5 +1,5 @@
 //special Parts
-var specialParts = ["storchkopf", "helmspechtkopf", "palmkakadubeine", "bekassineschwanz", "schnurrvogelfluegel", "blaukopfschmetterlingbeine"]
+var specialParts = ["waldkauz" , "storchkopf", "helmspechtkopf", "palmkakadubeine", "bekassineschwanz", "schnurrvogelfluegel", "blaukopfschmetterlingbeine"]
 var timer = 0;
 var receivedString = ["wings","none","none","0"];
 var lastReceivedString = ["yomoma"];
@@ -10,7 +10,7 @@ var lastReceivedString = ["yomoma"];
 function findSpecialPart(column, bird, part) {
   if (specialParts.indexOf(bird + part) > -1) {
     console.log("is Special Part")
-    $(column).children(".iconBox").css("background-color", "#6CC7B4")
+    //$(column).children(".iconBox").css("background-color", "#6CC7B4")
 
   } else {
     console.log("is not Special Part")
@@ -24,11 +24,68 @@ function getContent(bird, part) {
   if (bird == "helmspecht") {
 
     if (part == "kopf") {
-      return ("DER KOPF VOM SPECHT IST SPEZIELL");
-    } else {
-      return ("DER SPECHT SELBST IST NICHT SPEZIELL");
+      return ("Mit erheblichem Kraftaufwand und Ausdauer klopfen Spechte mit dem Schnabel gegen Baumstämme um Futter zu finden, Nisthöhlen zu zimmern, ihr Revier zu markieren oder Geschlechtspartner anzuziehen.");
     }
-  } else {
+  }
+  else if (bird == "storch"){
+    if (part == "kopf") {
+      return ("Er verständigt sich durch Klappern mit dem Schnabel, deshalb wird er auch Klapperstorch genannt. Geklappert wird zur Begrüßung des Partners am Nest und zur Verteidigung gegen Nestkonkurrenten. Auch beim Balzritual wird geklappert.");
+    }
+  }
+
+  else if (bird == "blaukopfschmetterling"){
+    if (part == "beine") {
+      return ("«Dribbeln» – Die Schnurrvögel machen einen Paartanz, Zweige im Schnabel haltend. Hochgeschwindigkeitskameras zeigen, dass Ihre Füsse zwischen fünfundzwanzig und fünfzig Mal pro Sekunde klopfen.");
+    }
+  }
+
+  else if (bird == "schnurrvogel"){
+    if (part == "fluegel") {
+      return ("«Flügeltöne» – Der Schnurrvogel erzeugt die hohen, «pieps»-artigen Töne mit seinen extrem modifizierten Sekundärschwingungen einen mechanischen Klang. Dieser Effekt ist als Sonation bekannt.");
+    }
+  }
+
+  else if (bird == "waldkauz"){
+    if (part == "fluegel") {
+      return ("«Schnabelknappen» – Waldkäuze verfügen über ein grosses Lautrepertoire mit stark variierender Lautstärke und Klangfarbe. Sie verfügen über wenige Instrumentallaute wie das Schnabelknappen bei aggressiver Erregung.");
+    }
+  }
+
+  else if (bird == "bekassine"){
+    if (part == "schwanz") {
+      return ("«Wummern» oder «Meckern» – ist ein Instrumentallaut, der durch die speziell versteiften, äusseren Steuerfedern erzeugt wird. Er ist während der Balzflüge vor allem in der Morgen- und Abenddämmerung zu hören.");
+    }
+    else if (part == "kopf") {
+      return ("Mit ihrem langen Schnabel stochern die Bekassinen tief im Untergrund oder Wasser und schreiten dabei langsam vorwärts.");
+    }
+    else if (part == "beine") {
+      return ("Die Bekassine mit ihren kräftigen Beinen haltet sich gerne in Hochmooren, Sumpfland, Nassbrachen und Feuchtwiesen auf.");
+    }
+    else if (part == "fluegel") {
+      return ("Bekassinen sind sowohl Kurz- und Langstreckenzieher. Sie halten sich im Sommer in Nordeuropa auf und im Winter in Mitteleuropa oder sie ziehen sogar weiter bis zum Äquator.");
+    }
+  }
+
+  else if (bird == "palmkakadu"){
+    if (part == "schwanz") {
+      return ("Der Palmkakadu hat einen langen Schwanz. Insgesamt hat er ein schwarzes Gefieder. Er bedeckt sein Körperkleid aber häufig mit Puder, so erscheint sein Gefieder blaugrau.");
+    }
+    else if (part == "kopf") {
+      return ("Die Schwarzfärbung des Kakadus erfolgt erst in einem Lebensalter von achtzehn bis vierundzwanzig Monaten. Vorher ist er hornfarben. Seine Zunge ist auffällig rot-schwarz.");
+    }
+    else if (part == "beine") {
+      return ("«Klopfgeräusche» – Mit Hilfe von Ästen, Steinen oder einem großen Samen schlagen sie auf einen abgestorbenen Baum und erzeugen damit hallende Klopfgeräusche. Es ist ein Markierverhalten.");
+    }
+    else if (part == "fluegel") {
+      return ("Sein Flug wirkt schwerfällig. Dies ist auf die weit ausholenden Flügelschläge zurückzuführen. Er ist außerdem ein guter Segler und biegt im Segelflug die Flügel abwärts.");
+    }
+  }
+
+
+
+
+
+  else {
     return ("vogel nicht gefunden")
 
   }
@@ -78,6 +135,9 @@ function makeChanges(column, bird, part) {
   if (bird == "blaukopfschmetterling") {
     bird = "blaukopf-\nschmetterling";
   }
+  if (bird == "storch") {
+    bird = "weissstorch";
+  }
   $(column).children(".nameBox").children("h1").text(bird);
 
 
@@ -113,7 +173,8 @@ client.on('message', function(topic, message) {
     console.log(receivedString);
   }
   else if (topic == "activeSound") {
-    console.log(message.toString());
+    soundVisual(message.toString());
+
   }
 
 });
@@ -139,6 +200,22 @@ function timerCount() {
 };
 
 function soundVisual(connector){
+  if (connector == "head") {
+    var column = ".birdHead";
+  } else if (connector == "body") {
+    var column = ".birdBody";
+  } else if (connector == "feet") {
+    var column = ".birdFeet";
+  } else if (connector == "wings") {
+    var column = ".birdWings";
+  } else if (connector == "tail") {
+    var column = ".birdTail";
+  } else {
+    console.log("!! connector name not recognized !!")
+  }
+  console.log(connector);
+  $(column).children(".iconBox").children(".backgroundIcon").fadeIn(500).delay(200).fadeOut(1000);
+
 
 }
 
